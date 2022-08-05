b4211617ebd090420e574fb76adddb9be8ceec4b
trung <tientrung14122012@gmail.com>
2022-08-05 15:43:02 +0700

    ss

---
+++ index.js
@@ -0,0 +1,7 @@
+// git config --global user.name "trung"
+// git config --global user.email "tientrung14122012@gmail.com"
+// git init khởi tạo kho chung
+// git status kiểm tra trạng thái các thư mục
+//  ( thử mục đang làm việc --------- staring Area--------- commited ) cập nhật khi dùng 
+//git add index.html-----------------index.html---------------------
+//git commit -m""-------------------------------------------index.html

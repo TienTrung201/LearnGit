5007c4d681ab2c85d4c1bb423ee754c76b14aa80
Xuan Phuc <dev.xuanphuc@gmail.com>
2023-01-18 09:44:09 +0700

    xuanphuccc: commit 1

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 // git config --global user.email "tientrung14122012@gmail.com"
 // git init khởi tạo kho chung
 // git status kiểm tra trạng thái các thư mục
-//  ( thử mục đang làm việc --------- staring Area--------- commited ) cập nhật khi dùng 
+//  ( thử mục đang làm việc --------- staring Area--------- commited ) cập nhật khi dùng
 //git add index.html-----------------index.html---------------------
 //git commit -m""-------------------------------------------index.html
 //git log : kiểm tra ấn Q để thoát git log
 // sau đó add git lên server?git branch -M main
 // git checkout -b body-style git branch (body-style tên branch)
-//  
+//
+
+// Xuanphuccc: Test git branch

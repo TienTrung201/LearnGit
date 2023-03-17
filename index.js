e71064cc009f50c621efe05495340c0b556b2546
trung <tientrung14122012@gmail.com>
2023-03-17 07:32:57 +0700

    main

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // git config --global user.name "trung"
 // git config --global user.email "tientrung14122012@gmail.com"
 // git init khởi tạo kho chung
+
+
 // git status kiểm tra trạng thái các thư mục
 //  ( thử mục đang làm việc --------- staring Area--------- commited ) cập nhật khi dùng 
 //git add index.html-----------------index.html---------------------
+// git restore --staged
 //git commit -m""-------------------------------------------index.html
 //git log : kiểm tra ấn Q để thoát git log
 // sau đó add git lên server?git branch -M main
-// git checkout -b body-style git branch (body-style tên branch)
-//  
+
+
+
+
+//git branch   kiểm tra có những branch nào
+// git checkout -b body-style git branch (body-style tên branch và trỏ vào body-style vàa body-style)
+// git checkout main di chuyển breach lại vào main vì main đã có sẵn k cần -b
+//   git merge body-style  chuyển code ở branch  main2 vào main
